9cd5430ee34ff267751e29de66fa5bf7ba3084cc
DSHuerta <davehuerta1231@outlook.com>
2025-11-28 23:11:42 -0800

    File containing configuration settings for firebase

---
+++ JavaScript/firebaseConfig.js
@@ -0,0 +1,10 @@
+// Enter your firebase config below
+export const firebaseConfig = {
+  apiKey: "AIzaSyDGsH-wE6-DBE0q0CUjFjkqYzFGaje0KFs",
+  authDomain: "team-alpha-quiz-db-79c1f.firebaseapp.com",
+  projectId: "team-alpha-quiz-db-79c1f",
+  storageBucket: "team-alpha-quiz-db-79c1f.firebasestorage.app",
+  messagingSenderId: "1081382156789",
+  appId: "1:1081382156789:web:f73623780655702d4648bc",
+  measurementId: "G-6LFQZZNV8L"
+};
